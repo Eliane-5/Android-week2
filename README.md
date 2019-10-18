d7361c608fc91fedb6874b2c952194408c34e798
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-18 18:57:55 +0200

    initial commit with old version

---
+++ README.md
@@ -0,0 +1,25 @@
+# Crafty Pictures
+
+#### Crafty pictures is a mobile application in development that will allow it's user to either post their pictures artframes on this platform and be able to sell them or buy other items via that same platform.
+
+#### By **Kamana Irankunda Eliane**
+
+## Prerequisites
+* [Here is a link to help with the installations of dependencies needed](https://github.com/Ajuajmal/Install-Config/wiki/Installation-Of-Android-Studio-In-Ubuntu-18.04#install-intels-kvm-for-better-avd-performance) 
+###### After installations
+* clone the repository and copy the link url
+* in android studio projects folder open the terminal
+* write git clone (copy the url you cloned)
+
+## Technologies Used
+* Android
+* XML
+* JAVA
+### Support and contact details
+May you encounter any issue or have any questions and suggestions please contact me at irankundaeliane9@gmail.com
+
+### License
+I hereby grant all access to everyone to view and use the code in this repository as they wish. Copyright (c) 2019 **Eliane**
+
+### comments
+[see this website for more information on custom buttons](https://www.codebrainer.com/blog/13-designs-for-buttons-every-android-beginner-should-know)
---
+++ app/src/main/java/com/moringaschool/craftypictures/ChoiceActivity.java
@@ -0,0 +1,50 @@
+package com.moringaschool.craftypictures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ChoiceActivity extends AppCompatActivity {
+    @BindView(R.id.userNameTextView) TextView mUserNameTextView;
+    @BindView(R.id.blackAndWhite) Button mblackAndWhiteButton;
+    @BindView(R.id.colorful) Button mColorfulButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+        mUserNameTextView.setText("Hello "+  userName +" have fun!");
+
+        mblackAndWhiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChoiceActivity.this, BlackAndWhiteActivity.class);
+                startActivity(intent);
+                Toast.makeText(ChoiceActivity.this, "Black and White pictures it is!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mColorfulButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChoiceActivity.this, ColorfulActivity.class);
+                startActivity(intent);
+                Toast.makeText(ChoiceActivity.this, "Colorful pictures it is!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
