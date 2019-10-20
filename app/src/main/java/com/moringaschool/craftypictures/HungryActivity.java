9d50f86b75769d611ca08baa8d57a305c4111588
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-20 16:13:01 +0200

    catching intent in hungry activity

--- app/src/main/java/com/moringaschool/craftypictures/HungryActivity.java
+++ app/src/main/java/com/moringaschool/craftypictures/HungryActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.craftypictures;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HungryActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class HungryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hungry);
+
+        Intent intent = getIntent();
     }
 }
